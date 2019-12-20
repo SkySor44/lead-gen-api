1c0687f760a345c7ec69368fe605e1a149fc8eb5
Skyler Sorensen <skylersorensen11@gmail.com>
2019-12-19 22:21:12 -0700

    make it better

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
-require('dotenv').config();
+const dotenv = require('dotenv');
 
+dotenv.config();
 app.get('/', (req, res) => {
+  console.log('here');
   // using Twilio SendGrid's v3 Node.js Library
   // https://github.com/sendgrid/sendgrid-nodejs
   const sgMail = require('@sendgrid/mail');
   sgMail.setApiKey(process.env.SENDGRID_API_KEY);
   const msg = {
-    to: 'test@example.com',
-    from: 'test@example.com',
+    to: 'tester@gmail.com',
+    from: 'tester@gmail.com',
     subject: 'Sending with Twilio SendGrid is Fun',
     text: 'and easy to do anywhere, even with Node.js',
     html: '<strong>and easy to do anywhere, even with Node.js</strong>'
