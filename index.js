6c3579f086edeec9f6be2823cf7c26f3f5897f30
Skyler Sorensen <skylersorensen11@gmail.com>
2019-12-19 22:33:45 -0700

    return hello world

--- index.js
+++ index.js
@@ -4,19 +4,20 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 app.get('/', (req, res) => {
-  console.log('here');
-  // using Twilio SendGrid's v3 Node.js Library
-  // https://github.com/sendgrid/sendgrid-nodejs
-  const sgMail = require('@sendgrid/mail');
-  sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-  const msg = {
-    to: 'tester@gmail.com',
-    from: 'test@gmail.com',
-    subject: 'Sending with Twilio SendGrid is Fun',
-    text: 'and easy to do anywhere, even with Node.js',
-    html: '<strong>and easy to do anywhere, even with Node.js</strong>'
-  };
-  sgMail.send(msg);
+  return 'Hello World';
+  // console.log('here');
+  // // using Twilio SendGrid's v3 Node.js Library
+  // // https://github.com/sendgrid/sendgrid-nodejs
+  // const sgMail = require('@sendgrid/mail');
+  // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+  // const msg = {
+  //   to: 'tester@gmail.com',
+  //   from: 'test@gmail.com',
+  //   subject: 'Sending with Twilio SendGrid is Fun',
+  //   text: 'and easy to do anywhere, even with Node.js',
+  //   html: '<strong>and easy to do anywhere, even with Node.js</strong>'
+  // };
+  // sgMail.send(msg);
 });
 
 app.listen(8000, () => {
